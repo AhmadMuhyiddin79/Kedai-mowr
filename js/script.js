d865cb02e0211e787ef0b1f6be5a6fdc3784d983
Ahmad Muhyiddin <syaukimuhammad26@gmail.com>
2023-06-12 19:56:17 -0700

    First Commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Toggle class active
+const navbarnav = document.querySelector(".navbar-nav");
+// Ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarnav.classList.toggle("active");
+};
+
+// klik diluar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+    navbarnav.classList.remove("active");
+  }
+});
